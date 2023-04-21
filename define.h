416e507bff72aa992526e005ebc5ffd3fda6bc59
ludmilaprolygin <71335675+ludmilaprolygin@users.noreply.github.com>
2023-04-21 16:11:51 -0300

    Update define.h
    Agregué algunos macros; sacamos los que no te parezcan #obvs

--- define.h
+++ define.h
@@ -1,4 +1,16 @@
 #ifndef DEFINE_H_INCLUDED
 #define DEFINE_H_INCLUDED
 
+// Macros para manejo de valores especiales
+#define FALSE           0
+#define TRUE            1
+#define POS_NULA        NULL
+#define ELE_NULO        NULL
+
+// Macros con códigos de finalización
+#define LST_NO_EXISTE_SIGUIENTE     2
+#define LST_NO_EXISTE_ANTERIOR      3
+#define LST_POSICION_INVALIDA       4
+#define LST_ERROR_MEMORIA           5
+
 #endif
