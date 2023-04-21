5e87c61c6b498aeffcc2f0137e094dbb29d12a84
ludmilaprolygin <71335675+ludmilaprolygin@users.noreply.github.com>
2023-04-21 15:38:24 -0300

    Create cuentapalabras.c

---
+++ cuentapalabras.c
@@ -0,0 +1,13 @@
+#include "listaordenada.h"
+#include "multiset.h"
+
+// Definición de macro de error para manejar errores en la apertura de archivos.
+#define ARCH_ERROR_APERTURA -1
+
+// -----------------------------------------------------------------------------
+/**
+ Robado del proyecto de 2020; la consigna era contar la cantidad de apariciones de una palabra de un archivo de texto.
+ - Cantidad apariciones: permite ingresar una palabra y muestra la cantidad de veces que dicha palabra se encuentra en el archivo.
+ 
+--> No es igual, pero podemos robar bastante. Lee un archivo y carga las palabras a un mapeo; podemos adecuarlo a nuestros TDA.
+**/
