a8b11c00fb13b18f348ff20aa8009c1bd9272219
ludmilaprolygin <71335675+ludmilaprolygin@users.noreply.github.com>
2023-04-21 14:30:26 -0300

    Update listaordenada.h
    Agregué documentación, macros con código de finalización y valores espciales (podemos borrarlos, pero para que queden a mano si llegamos a quererlos).  Comenté líneas 1 a 3 porque CREEEEEEEEEEEEO que van en el .c únicamente.
    
    Agregué la palabra reservada "extern" a los métodos.

--- listaordenada.h
+++ listaordenada.h
@@ -1,27 +1,83 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "define.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include "define.h"
 
-typedef enum {
-ELEM1_MAYOR_QUE_ELEM2,
-ELEM1_IGUAL_QUE_ELEM2,
-ELEM1_MENOR_QUE_ELEM2
+#ifndef LISTA_H_INCLUDED
+#define LISTA_H_INCLUDED
+
+// Macros para manejo de valores especiales
+#define FALSE           0
+#define TRUE            1
+#define POS_NULA        NULL
+#define ELE_NULO        NULL
+
+// Macros con códigos de finalización
+#define LST_NO_EXISTE_SIGUIENTE     2
+#define LST_NO_EXISTE_ANTERIOR      3
+#define LST_POSICION_INVALIDA       4
+#define LST_ERROR_MEMORIA           5
+
+typedef enum 
+{
+  ELEM1_MAYOR_QUE_ELEM2,
+  ELEM1_IGUAL_QUE_ELEM2,
+  ELEM1_MENOR_QUE_ELEM2
 } comparacion_resultado_t;
-struct elemento {
-int a;
-char *b;
+
+struct elemento 
+{
+  int a;
+  char *b;
 };
+
+//Structs para la manipulación de la lista
 typedef struct elemento elemento_t;
+
 typedef comparacion_resultado_t (funcion_comparacion_t) (
-elemento_t *elem1, elemento_t *elem2
+  elemento_t *elem1, elemento_t *elem2
 );
+
 typedef struct lista lista_t;
 
+// ---------------------------------------------------------------------------------------------
+// Definición de prototipos y documentación de las funciones.
+// ---------------------------------------------------------------------------------------------
+
 //Metodos TDA lista.
-lista_t *lista crear();
-int lista_insertar(lista t *l, elemento_t elem, unsigned int pos); 
-elemento_t *lista_eliminar(lista_t *l, unsigned int pos); 
-elemento_t *lista_elemento(lista_t *l, unsigned int pos); 
-int lista_ordenar(lista_t *l, funcion comparacion_t comparar);
-unsigned int lista_cantidad(lista_t *l); 
-int lista_vacia(list_t lista);
+
+/**
+ Crea una lista vacía y la devuelve.
+**/
+extern lista_t *lista crear();
+
+/**
+ Inserta el elemento elem en la posición pos de la lista.
+**/
+extern int lista_insertar(lista t *l, elemento_t elem, unsigned int pos);
+
+/**
+Elimina el elemento de la posición pos de la lista.
+**/
+extern elemento_t *lista_eliminar(lista_t *l, unsigned int pos); 
+
+/**
+ Devuelve un puntero al elemento que ocupa la posición pos de la lista.
+**/
+extern elemento_t *lista_elemento(lista_t *l, unsigned int pos); 
+
+/**
+Dada la lista l y la función comparar ordena la lista de acuerdo al criterio de dicha función.
+**/
+extern int lista_ordenar(lista_t *l, funcion comparacion_t comparar);
+
+/**
+Devuelve la cantidad de elementos de la lista l.
+**/
+extern unsigned int lista_cantidad(lista_t *l); 
+
+/**
+Devuelve verdadero (̸= 0) si la lista está vacía, y falso (= 0) si la lista contiene al menos un elemento.
+**/
+extern int lista_vacia(list_t lista);
+
+#endif // LISTA_H_INCLUDED
