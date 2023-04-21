967eb89ef699f824a92d43fe927c28a04a6b28cd
ludmilaprolygin <71335675+ludmilaprolygin@users.noreply.github.com>
2023-04-21 16:09:17 -0300

    Update listaordenada.h
    Borré los #define

--- listaordenada.h
+++ listaordenada.h
@@ -5,18 +5,6 @@
 #ifndef LISTA_H_INCLUDED
 #define LISTA_H_INCLUDED
 
-// Macros para manejo de valores especiales
-#define FALSE           0
-#define TRUE            1
-#define POS_NULA        NULL
-#define ELE_NULO        NULL
-
-// Macros con códigos de finalización
-#define LST_NO_EXISTE_SIGUIENTE     2
-#define LST_NO_EXISTE_ANTERIOR      3
-#define LST_POSICION_INVALIDA       4
-#define LST_ERROR_MEMORIA           5
-
 typedef enum 
 {
   ELEM1_MAYOR_QUE_ELEM2,
