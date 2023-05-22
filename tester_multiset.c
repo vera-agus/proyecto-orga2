6499cdf57eed0f20bd7f9a16ba23a8433198af53
ludmilaprolygin <mlprolygin@gmail.com>
2023-05-22 13:47:54 -0300

    tester_multiset
    El error en la carga de los elementos de los archivos no es por el multiset; ver algo en la lectura o manipulación del registro. Multiset anda bien

--- tester_multiset.c
+++ tester_multiset.c
@@ -75,4 +75,77 @@ int main()
         printf("borra OK");
 
     return 0;
+
+    /*
+    //TEST CON MULTISET D: POR QUE NO ANDÁAAAAAAAAAAAAA
+    multiset_t* m = multiset_crear(); // OK - Finaliza con 0
+    char* p1 = "uno";
+    char* p2 = "dos";
+    char* p3 = "tres";
+    char* p4 = "dos";
+    char* p5 = "uno";
+
+    char* p6 = "dos";
+    char* p7 = "treinta";
+    char* p8 = "tres";
+    char* p9 = "treinta";
+
+    char* p10 = "tres";
+    char* p11 = "doce";
+    char* p12 = "treinta";
+    char* p13 = "treinta";
+    char* p14 = "ocho";
+
+    char* p15 = "dos";
+    char* p16 = "treinta";
+    char* p17 = "ocho";
+    char* p18 = "ocho";
+
+
+    // int cant1, cant2, cant3, cant4, cant5, cantUN;
+
+    multiset_insertar(m, p1);
+    multiset_insertar(m, p2);
+    multiset_insertar(m, p3);
+    multiset_insertar(m, p4);
+    multiset_insertar(m, p5);
+    multiset_insertar(m, p6);
+    multiset_insertar(m, p7);
+    multiset_insertar(m, p8);
+    multiset_insertar(m, p9);
+    multiset_insertar(m, p10);
+    multiset_insertar(m, p11);
+    multiset_insertar(m, p12);
+    multiset_insertar(m, p13);
+    multiset_insertar(m, p14);
+    multiset_insertar(m, p15);
+    multiset_insertar(m, p16);
+    multiset_insertar(m, p17);
+    multiset_insertar(m, p18);
+
+    /*
+    cant1 = multiset_cantidad(m, p1);
+    cant2 = multiset_cantidad(m, p2);
+    cant3 = multiset_cantidad(m, p3);
+    cant4 = multiset_cantidad(m, p4);
+    cant5 = multiset_cantidad(m, p5);
+    cantUN = multiset_cantidad(m, un); // OK - Finaliza con 0 y computa bien
+
+    printf("p1 aparece %i veces, \np2 aparece %i veces, \np3 aparece %i veces, \np4 aparece %i veces, \np5 aparece %i veces \n\n", cant1, cant2, cant3, cant4, cant5);
+    */
+
+    lista_t l = multiset_elementos(m, comparar_multiset);
+    int cant = lista_cantidad(&l);
+    printf("\n");
+    for(int i = 0; i < cant; i++)
+        printf("%i %s\n", lista_elemento(&l, i)->a, lista_elemento(&l, i)->b);
+    printf("\n");
+
+    multiset_eliminar(&m); // OK - Finaliza con 0 y setea a m en NULL
+
+    if(m == NULL)
+        printf("borra OK");
+
+    return 0;
+    */
 }
